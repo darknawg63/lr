59d6558dda65ff44b92cda4824e4fe3aab419030
darknawg63 <darknawg63@gmail.com>
2015-03-14 21:38:17 +0100

    Register & Login (Part 14): Email Activation (Part 3)

---
+++ activate.php
@@ -0,0 +1,22 @@
+<?php
+include 'core/init.php';
+logged_in_redirect();
+include 'includes/overall/header.php';
+
+if(isset($_GET['email'], $_GET['email_code']) === true) {
+	$email 		= trim($_GET['email']);
+	$email_code = trim($_GET['email_code']);
+	
+	if(email_exists($email) === false) {
+		$errors[] = 'Oops, something went wrong, and we couldn\'t find this email address!';
+	} else if(activate($email, $email_code) === false) {
+		$errors[] = 'We had problems activating your account.';
+	}
+	
+} else {
+	header('Location: index.php');
+	exit();
+}
+
+include 'includes/overall/footer.php';
+?>
--- core/functions/users.php
+++ core/functions/users.php
@@ -1,4 +1,17 @@
 <?php
+function activate($email, $email_code) {
+	$email 		= mysql_real_escape_string($email);
+	$email_code = mysql_real_escape_string($email_code);
+	
+	if(mysql_result(mysql_query("SELECT COUNT(`user_id`) FROM `users` WHERE `email` = '$email' AND `active` = 0"), 0) == 1) {
+		mysql_query("UPDATE `users` SET `active` = 1 WHERE `email` = '$email'");
+		
+		return true;
+	} else {
+		return false;
+	}
+}
+
 function change_password($user_id, $password) {
 	$user_id = (int)$user_id;
 	$password = md5($password);
