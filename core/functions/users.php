89e805a5179fb698f8c33c74761c0e031af6c878
darknawg63 <darknawg63@gmail.com>
2015-03-17 13:39:08 +0100

    Register & Login (Part 19): Forgotten Password (Part 1)

--- core/functions/users.php
+++ core/functions/users.php
@@ -3,12 +3,14 @@ function recover($mode,$email) {
 	$mode 	= sanitize($mode);
 	$email 	= sanitize($email);
 	
-	$user_data = user_data(user_id_from_email($email), 'first_name', 'username');
+	$user_data = user_data(user_id_from_email($email), 'user_id', 'first_name', 'username');
 	
 	if($mode == 'username') {
 		email($email, 'Your username', "Hello " . $user_data['first_name'] . ",\n\nYour username is: " . $user_data['username'] . "\n\n-phpacademy");
 	} else if($mode == 'password') {
-		//recover password
+		$generated_password = substr(md5(rand(999, 999999)), 0, 8);
+		change_password($user_data['user_id'], $generated_password);
+		email($email, 'Your password recovery', "Hello " . $user_data['first_name'] . ",\n\nYour new password is: " . $generated_password . "\n\n-phpacademy");
 	}
 }
 
