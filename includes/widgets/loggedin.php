e64494f6747962f2555b466565c8e77d3db2fd4c
darknawg63 <darknawg63@gmail.com>
2015-03-21 14:37:33 +0100

    Register & Login (Part 23): Profile Image Upload (Part 1)

--- includes/widgets/loggedin.php
+++ includes/widgets/loggedin.php
@@ -1,6 +1,16 @@
 <div class="widget">
     <h2>Hello, <?php echo $user_data['first_name']; ?>!</h2>
     <div class="inner">
+		<div class="profile">
+			<?php 
+			if(empty($user_data['profile']) === false) {
+				echo '<img src="', $user_data['profile'] , '" alt="', $user_data['first_name'] , '\s Profile Image">';
+			}
+			?>
+			<form action="" method="post" enctype="multipart/form-data">
+				<input type="file" name="profile"> <input type="submit">
+			</form>			
+		</div>
 		<ul>
 			<li>
 				<a href="logout.php">Log out</a> 
